a9426d8abfe854e5ea698fc00a92822ce1506246
Cristales <sirsly073@gmail.com>
2023-05-07 22:37:00 -0600

    Screen Component

--- calculadora/src/components/Display/Display.jsx
+++ calculadora/src/components/Display/Display.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from './Display.module.css'
+import PropTypes from 'prop-types'
 
-const Display = () => {
+const Display = ( { value, theme } ) => {
   return (
-    <div>Display</div>
+    <div className={`${styles.screen} ${styles[`screen--${theme}`]}`}>
+      {value}
+    </div>
   )
+}
+
+Display.propTypes = {
+  value: PropTypes.string.isRequired,
+  theme: PropTypes.oneOf(['light', 'dark'])
 }
 
 export default Display
---
+++ calculadora/src/components/Display/Display.stories.js
@@ -0,0 +1,15 @@
+import Display from "./Display"
+
+export default {
+    title: 'Calculator/Display',
+    component: Display,
+    tags: ['autodocs']
+}
+
+export const Light = {
+    theme: 'light'
+}
+
+export const Dark = {
+    theme: 'dark'
+}
