ee654768349edb58a3a5d575c0b4296c94def1a1
Cristales <sirsly073@gmail.com>
2023-05-07 21:44:56 -0600

    ESlint Config

--- calculadora/.eslintrc.cjs
+++ calculadora/.eslintrc.cjs
@@ -1,14 +1,32 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:storybook/recommended"],
-  "overrides": [],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  extends: [
+    'plugin:react/recommended',
+    'airbnb-base',
+    'airbnb/rules/react',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  "plugins": ["react"],
-  "rules": {}
-};
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "semi": [
+      "error",
+      "never"
+    ],
+    "max-len" : [
+      "warn",
+      {"code":120}
+    ],
+    "prettier/prettier": ["warn", {"semi":false, "tabWidth": 4}]
+  },
+}
