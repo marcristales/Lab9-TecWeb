cd78703a8351be5ddd9fdcb527eb0a9334c8db4f
Cristales <sirsly073@gmail.com>
2023-05-09 01:17:51 -0600

    Calculator v1.0
    Features:
    - Calculadora completa
    - Suma, resta, divide, multiplica, saca porcentaje, acepta decimales, acepta +-, AC elimina todo.
    - Solo acepta 9 digitos
    - Negativos y numeros con mas de 9 digitos tiran ERROR
    
    Falta:
    - Spam de igual hace la ultima operacion con el ultimo valor
    - Al mostrar resultado, marcar otro numero solo lo concatena al resultado en lugar de reemplazar el valor

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Calculator from './components/Calculator/Calculator'
 import Display from './components/Display/Display'
-import { useState } from 'react';
-import Keyboard from './components/Keyboard/Keyboard';
-import Button from './components/Button/Button';
+import { useState } from 'react'
+import Keyboard from './components/Keyboard/Keyboard'
+import Button from './components/Button/Button'
 
 function App() {
   const [theme, setTheme] = useState('dark')
+  const [inputValue, setInputValue] = useState('0')
+  const [previousValue, setPreviousValue] = useState(null)
+  const [operator, setOperator] = useState(null)
 
   const botones = [
     ['AC', '±', '%', '÷'],
@@ -20,6 +23,70 @@ function App() {
     setTheme(theme === 'light' ? 'dark' : 'light')
   }
 
+  const handleButtonClick = (buttonValue) => {
+    switch (buttonValue) {
+      case "AC":
+        setInputValue("0")
+        setPreviousValue(null)
+        setOperator(null)
+        break
+      case "±":
+        setInputValue((inputValue) => (inputValue.startsWith("-") ? inputValue.slice(1) : "-" + inputValue))
+        break
+      case "%":
+        setInputValue((inputValue) => (parseFloat(inputValue) / 100).toString())
+        break
+      case ".":
+        if (!inputValue.includes(".")) {
+          setInputValue((inputValue) => inputValue + ".")
+        }
+        break
+      case "÷":
+      case "x":
+      case "-":
+      case "+":
+        setOperator(buttonValue)
+        setPreviousValue(inputValue)
+        setInputValue("0")
+        break
+      case "=":
+        if (operator && previousValue && inputValue) {
+          let result
+          switch (operator) {
+            case "÷":
+              result = parseFloat(previousValue) / parseFloat(inputValue)
+              break
+            case "x":
+              result = parseFloat(previousValue) * parseFloat(inputValue)
+              break
+            case "-":
+              result = parseFloat(previousValue) - parseFloat(inputValue)
+              result = result < 0 ? 'ERROR' : result
+              break
+            case "+":
+              result = parseFloat(previousValue) + parseFloat(inputValue)
+              break
+            default:
+              break
+          }
+          const resultString = result.toString()
+          if (resultString.length > 9) {
+            setInputValue('ERROR')
+          } else {
+            setInputValue(resultString)
+          }
+          setPreviousValue(null)
+          setOperator(null)
+        }
+        break
+      default:
+        if (inputValue.length < 9) {
+          setInputValue((inputValue === "0" ? buttonValue : inputValue + buttonValue))
+        }
+        break
+    }
+  }
+
   return (
     <>
       <div className='cambiarTema'>
@@ -28,7 +95,7 @@ function App() {
       <div className='container'>
         <div className='calculadora'>
           <Calculator theme={theme}>
-            <Display value='0' theme={theme} />
+            <Display value={inputValue} theme={theme} />
             <Keyboard>
               {botones.flat().map((boton, index) => {
                 return (
@@ -36,9 +103,7 @@ function App() {
                     key={index}
                     type={boton === '=' ? 'equals' : ''}
                     value={boton}
-                    onClick={() => {
-                      console.log("Button clicked!");
-                    }}
+                    onClick={() => handleButtonClick(boton)}
                     theme={theme}
                   />
                 )
